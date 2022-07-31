70b1945ea2ab1ea444bf2ba3b2cba8f2c506f2ed
seonhwan <se0o0ver@gmail.com>
2022-07-31 19:08:29 +0900

    modify diag nav

--- src/components/Diagnosis/DiagnosisNav.jsx
+++ src/components/Diagnosis/DiagnosisNav.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./css/diagnosis.module.css";
 
 const DiagnosisNav = () => {
-  const DiagnosisNavList  = [
-    {content: "현재상태 점검하기", href:"."},
-    {content: "진단결과 보기", href:"./result"}
-  ];
+    const DiagnosisNavList = [
+        { id: 1, content: "현재상태 점검하기", href: "." },
+        { id: 2, content: "진단결과 보기", href: "./result" },
+    ];
+    const [currentTab, setCurrentTab] = useState(0);
+    const handleMenu = (idx) => {
+        setCurrentTab(idx);
+    };
 
-  return (
-    <div>
-      <nav className={styles.diaNav}>
-        <ul>
-          {DiagnosisNavList.map((element, index)=>{
-            return(
-              <li key={index}>
-                <NavLink to={element.href}className={({ isActive }) =>
-                                    isActive
+    return (
+        <div>
+            <nav className={styles.diaNav}>
+                <ul>
+                    {DiagnosisNavList.map((element, index) => {
+                        return (
+                            <li
+                                key={element.id}
+                                className={
+                                    currentTab === index
                                         ? `${styles.navbtn} ${styles.on}`
                                         : `${styles.navbtn}`
-                                }>{element.content}
-                </NavLink>
-              </li>
-            )
-          })}
-        </ul>
-      </nav>
-
-    </div>
-  );
+                                }
+                                onClick={() => handleMenu(index)}
+                                type="button"
+                            >
+                                <NavLink to={element.href}>
+                                    {element.content}
+                                </NavLink>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </div>
+    );
 };
 
 export default DiagnosisNav;
